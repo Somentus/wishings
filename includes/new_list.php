ad853a4cdcd780ec833aceac28fc2b4629fb6877
Somentus <somentus@gmail.com>
2018-02-07 00:28:12 +0100

    Display new_list form input.

--- includes/new_list.php
+++ includes/new_list.php
@@ -1,1 +1,12 @@
 <?php
+
+require_once($_SERVER["DOCUMENT_ROOT"].'/includes/DB.php');
+$pdo = DB();
+
+if(isset($_POST['create_list'])) {
+	echo "List name: ".$_POST['list_name'].". List description: ".$_POST['list_description']."<br/>";
+	$items = $_POST['item'];
+	foreach($items as $item) {
+		echo "Item Name: ".$item['name'].". Item description: ".$item['description']."<br/>";
+	}
+}
